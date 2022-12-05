657ab164238707d625439794e7169c895eed1abe
Nick Vella <npvella@email.neit.edu>
2022-12-05 13:59:46 -0500

    end of class week 9 day 1

--- AsteroidAvoider/AsteroidAvoider.js
+++ AsteroidAvoider/AsteroidAvoider.js
@@ -1,11 +1,96 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var timer = requestAnimationFrame(main);
+var gameOver = false;
+var score = 0;
+var highScore = 0;
 
 //asteroid variables
 var numAsteroids = 20;
 var asteroids = [];
 
+//Player Ship Variables
+var ship = new PlayerShip();
+
+//create keyboard event handlers
+document.addEventListener("keydown", pressKeyDown);
+document.addEventListener("keyup", pressKeyUp);
+
+function pressKeyDown(e){
+    if(!gameOver){
+        if(e.keyCode == 87){
+            //code for up (W)
+            ship.up = true;
+        }
+        if(e.keyCode == 65){
+            //code for left (A)
+            ship.left = true;
+        }
+        if(e.keyCode == 68){
+            //code for right (D)
+            ship.right = true;
+        }
+        if(e.keyCode == 83){
+            //code for down (S)
+            ship.down = true;
+        }
+        //arrow keys
+        if(e.keyCode == 38){
+            //code for up
+            ship.up = true;
+        }
+        if(e.keyCode == 37){
+            //code for left
+            ship.left = true;
+        }
+        if(e.keyCode == 39){
+            //code for right
+            ship.right = true;
+        }
+        if(e.keyCode == 40){
+            //code for down
+            ship.down = true;
+        }
+    }
+}
+
+function pressKeyUp(e){
+    if(!gameOver){
+        if(e.keyCode == 87){
+            //code for up (W)
+            ship.up = false;
+        }
+        if(e.keyCode == 65){
+            //code for left (A)
+            ship.left = false;
+        }
+        if(e.keyCode == 68){
+            //code for right (D)
+            ship.right = false;
+        }
+        if(e.keyCode == 83){
+            //code for down (S)
+            ship.down = false;
+        }
+        //arrow keys
+        if(e.keyCode == 38){
+            //code for up
+            ship.up = false;
+        }
+        if(e.keyCode == 37){
+            //code for left
+            ship.left = false;
+        }
+        if(e.keyCode == 39){
+            //code for right
+            ship.right = false;
+        }
+        if(e.keyCode == 40){
+            //code for down
+            ship.down = false;
+        }
+    }
+}
 
 //Asteroid class
 function Asteroid(){
@@ -28,6 +113,66 @@ function Asteroid(){
     }
 }
 
+function PlayerShip(){
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+    this.width = 20;
+    this.height = 20;
+    this.up = false;
+    this.down = false;
+    this.left = false;
+    this.right = false;
+    this.vx = 0;
+    this.vy = 0;
+
+
+    this.drawShip = function(){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        
+        //ship style
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(0,-10);
+        ctx.lineTo(10,10);
+        ctx.lineTo(-10,10);
+        ctx.lineTo(0,-10);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    }
+
+    this.moveShip = function(){
+        this.x += this.vx;
+        this.y += this.vy;
+
+        //adding boundaries for screen
+        //bottom
+        if(this.y > canvas.height - this.height/2){
+            this.y = canvas.height - this.height/2;
+            this.vy = 0;
+        }
+
+        //top
+        if(this.y < this.height/2){
+            this.y = this.height/2;
+            this.vy = 0;
+        }
+        //right
+        if(this.x > canvas.width - this.width/2){
+            this.x = canvas.width - this.width/2;
+            this.vx = 0;
+        }
+
+        //left
+        if(this.x < this.width/2){
+            this.x = this.width/2;
+            this.vx = 0;
+        }
+    }
+
+}
+
 //for loop to instantiate asteroids for game
 for(var i = 0; i<numAsteroids; i++){
     asteroids[i] = new Asteroid();
@@ -37,7 +182,39 @@ function main(){
     //clear the canvas
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
+    //draw score to screen
+    ctx.save();
+    ctx.font = "15px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score: ${score}`, canvas.width - 150, 30);
+    ctx.restore();
+
+    //vertical movement
+    if(ship.up){
+        ship.vy = -10;
+    }else{
+        ship.vy = 3;
+    }
+    //horizontal movement
+    if(ship.left){
+        ship.vx = -3;
+    }else if(ship.right){
+        ship.vx = 3;
+    }else{
+        ship.vx=0;
+    }
     for(var i = 0; i<asteroids.length; i++){
+        var dX = ship.x - asteroids[i].x;
+        var dY = ship.y - asteroids[i].y;
+        var distance = Math.sqrt((dX*dX) + (dY*dY));
+
+        if(detectCollision(distance, (ship.height/2 + asteroids[i].radius))){
+            //console.log("Hit Asteroid");
+            //alert("Hit Asteroid");
+            gameOver = true;
+        }
+
+
         if(asteroids[i].y>canvas.height + asteroids[i].radius){
             asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) - canvas.height;
             asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius);
@@ -45,8 +222,19 @@ function main(){
         asteroids[i].y += asteroids[i].vy
         asteroids[i].drawAsteroid();
     }
-    //refresh the screen
-    timer = requestAnimationFrame(main);
+
+    //draw the ship
+    ship.moveShip();
+    ship.drawShip();
+    if(!gameOver){
+        //refresh the screen
+        timer = requestAnimationFrame(main);
+    }
+
+    while(asteroids.length < numAsteroids){
+        asteroids.push(new Asteroid());
+    }
+
 }
 
 //utility functions
@@ -54,3 +242,24 @@ function main(){
 function randomRange(high,low){
     return Math.random() * (high-low) + low;
 }
+
+
+function detectCollision(distance, calcDistance){
+    return distance < calcDistance;
+}
+
+function scoreTimer(){
+    if(!gameOver){
+        score++;
+
+        if(score % 5 == 0){
+            numAsteroids += 5;
+            console.log(numAsteroids);
+        }
+        //call scoreTimer every second
+        setTimeout(scoreTimer, 1000);
+    }
+}
+
+//temp call score function
+scoreTimer();
