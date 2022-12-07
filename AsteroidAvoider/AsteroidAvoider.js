0356a0b1acd93d5b84f8df72ec30f45b77b289fe
Nick Vella <npvella@email.neit.edu>
2022-12-07 14:34:25 -0500

    wsdefrgth

--- AsteroidAvoider/AsteroidAvoider.js
+++ AsteroidAvoider/AsteroidAvoider.js
@@ -1,265 +1,335 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var timer = requestAnimationFrame(main);
-var gameOver = false;
-var score = 0;
-var highScore = 0;
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext('2d')
+var timer = requestAnimationFrame(main)
+var ship
+var numAsteroids = 20
+var asteroids = []
+var gameOver = true
+var gameStates = []
+var currentState = 0
+var score = 0
+var highScore = 0
 
-//asteroid variables
-var numAsteroids = 20;
-var asteroids = [];
 
-//Player Ship Variables
-var ship = new PlayerShip();
+//utility functions
+function randomRange(high, low){
+    return Math.random() * (high-low) + low
+}
 
-//create keyboard event handlers
-document.addEventListener("keydown", pressKeyDown);
-document.addEventListener("keyup", pressKeyUp);
+function gameStart(){
+    //For Loop to create the instances of Asteroids
+    for(var i = 0; i < numAsteroids; i++){
+        asteroids[i] = new Asteroid()
+    }
+
+    //Create an instance of the PlayerShip
+    ship = new PlayerShip()
+}
+
+//Constructor Function for Asteroid Class
+function Asteroid(){
+    this.radius = randomRange(15,2)
+    this.x = randomRange(canvas.width - this.radius, this.radius)
+    this.y = randomRange(canvas.height - this.radius, this.radius) - canvas.height
+    this.vy = randomRange(10, 5)
+    this.color = "white"
+
+    this.drawAsteroid = function(){
+        ctx.save()
+        ctx.beginPath()
+        ctx.fillStyle = this.color
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true)
+        ctx.closePath()
+        ctx.fill()
+        ctx.restore()
+
+    }
+
+}
+
+
+
+//Setup Keyboard Event Handlers 
+document.addEventListener("keydown", pressKeyDown)
+document.addEventListener("keyup", pressKeyUp)
 
 function pressKeyDown(e){
-    if(!gameOver){
-        if(e.keyCode == 87){
-            //code for up (W)
-            ship.up = true;
+    if (!gameOver) {
+        if (e.keyCode == 87) {
+            ship.up = true
         }
-        if(e.keyCode == 65){
-            //code for left (A)
-            ship.left = true;
+        if (e.keyCode == 65) {
+            ship.left = true
         }
-        if(e.keyCode == 68){
-            //code for right (D)
-            ship.right = true;
+        if (e.keyCode == 68) {
+            ship.right = true
         }
-        if(e.keyCode == 83){
-            //code for down (S)
-            ship.down = true;
-        }
-        //arrow keys
-        if(e.keyCode == 38){
-            //code for up
-            ship.up = true;
-        }
-        if(e.keyCode == 37){
-            //code for left
-            ship.left = true;
-        }
-        if(e.keyCode == 39){
-            //code for right
-            ship.right = true;
-        }
-        if(e.keyCode == 40){
-            //code for down
-            ship.down = true;
+        if (e.keyCode == 83) {
+            ship.down = true
         }
     }
+
+    if(gameOver){
+
+        //checking for spacebar
+        if(e.keyCode == 32){
+            if(currentState == 2){
+                //game over screen reatarts game
+                currentState = 0
+                //resets number of asteroids
+                numAsteroids = 20
+                //empties asteroids array
+                asteroids = []
+                //resets score
+                score = 0
+                gameStart()
+                main()
+            }
+            else{
+                //main screen starts game 
+                gameStart()
+                currentState = 1
+                gameOver = false
+                main()
+                scoreTimer()
+                console.log("space")
+
+            }
+            
+        }
+    }
+    
 }
 
 function pressKeyUp(e){
     if(!gameOver){
-        if(e.keyCode == 87){
-            //code for up (W)
-            ship.up = false;
+        if (e.keyCode == 87) {
+            ship.up = false
         }
-        if(e.keyCode == 65){
-            //code for left (A)
-            ship.left = false;
+        if (e.keyCode == 65) {
+            ship.left = false
         }
-        if(e.keyCode == 68){
-            //code for right (D)
-            ship.right = false;
+        if (e.keyCode == 68) {
+            ship.right = false
         }
-        if(e.keyCode == 83){
-            //code for down (S)
-            ship.down = false;
-        }
-        //arrow keys
-        if(e.keyCode == 38){
-            //code for up
-            ship.up = false;
-        }
-        if(e.keyCode == 37){
-            //code for left
-            ship.left = false;
-        }
-        if(e.keyCode == 39){
-            //code for right
-            ship.right = false;
-        }
-        if(e.keyCode == 40){
-            //code for down
-            ship.down = false;
-        }
+        if (e.keyCode == 83) {
+            ship.down = false
+        } 
     }
+    
 }
 
-//Asteroid class
-function Asteroid(){
-    //properties to draw the asteroid
-    this.radius = randomRange(15,2);
-    this.x = randomRange(canvas.width - this.radius,this.radius);
-    this.y = randomRange(canvas.height - this.radius,this.radius) - canvas.height;
-    this.vy = randomRange(10,5);
-    this.color = "white";
-
-    //methods(functions) to draw asteroid
-    this.drawAsteroid = function(){
-        ctx.save();
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
-        ctx.closePath();
-        ctx.fill();
-        ctx.restore();
-    }
-}
-
+//constructor function
 function PlayerShip(){
-    this.x = canvas.width/2;
-    this.y = canvas.height/2;
-    this.width = 20;
-    this.height = 20;
-    this.up = false;
-    this.down = false;
-    this.left = false;
-    this.right = false;
-    this.vx = 0;
-    this.vy = 0;
-
+    this.x = canvas.width/2
+    this.y = canvas.height/2
+    this.w = 20
+    this.h = 20
+    this.vx = 0
+    this.vy = 0
+    this.up = false
+    this.down = false
+    this.left = false
+    this.right = false
+    this.flamelength = 30
 
     this.drawShip = function(){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        
-        //ship style
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.moveTo(0,-10);
-        ctx.lineTo(10,10);
-        ctx.lineTo(-10,10);
-        ctx.lineTo(0,-10);
-        ctx.closePath();
+       ctx.save()
+        ctx.translate(this.x, this.y)
+        if(this.up || this.left || this.right){
+            ctx.save()
+            //Changes the drawing values to animate the flame
+            if(this.flamelength == 30){
+                this.flamelength = 20
+                ctx.fillStyle = "yellow"
+            }else{
+                
+                this.flamelength = 30
+                ctx.fillStyle = "orange"
+            }
+            ctx.beginPath()
+            ctx.moveTo(0, this.flamelength)
+            ctx.lineTo(5,5)
+            ctx.lineTo(-5,5)
+            ctx.lineTo(0,this.flamelength)
+            ctx.closePath()
+            ctx.fill()
+            ctx.restore()
+
+        }
+        ctx.fillStyle = "red"
+        ctx.beginPath()
+        ctx.moveTo(0, -10)
+        ctx.lineTo(10, 10)
+        ctx.lineTo(-10, 10)
+        ctx.lineTo(0, -10)
+        ctx.closePath()
         ctx.fill();
-        ctx.restore();
+        ctx.restore() 
     }
 
-    this.moveShip = function(){
-        this.x += this.vx;
-        this.y += this.vy;
+    this.move = function(){
+        this.x += this.vx
+        this.y += this.vy
 
-        //adding boundaries for screen
-        //bottom
-        if(this.y > canvas.height - this.height/2){
-            this.y = canvas.height - this.height/2;
-            this.vy = 0;
+        //bottom boundary of screen
+        if(this.y > canvas.height - this.h/2){
+            this.y = canvas.height - this.h/2
+            this.vy = 0
+        }
+        //top boundary of screen
+        if(this.y < this.h/2){
+            this.y = this.h/2
+            this.vy = 0
         }
 
-        //top
-        if(this.y < this.height/2){
-            this.y = this.height/2;
-            this.vy = 0;
+        //right boundary of screen
+        if(this.x > canvas.width - this.w/2){
+            this.x = canvas.width - this.w/2
+            this.vx = 0
         }
-        //right
-        if(this.x > canvas.width - this.width/2){
-            this.x = canvas.width - this.width/2;
-            this.vx = 0;
-        }
-
-        //left
-        if(this.x < this.width/2){
-            this.x = this.width/2;
-            this.vx = 0;
+        //left boundary of screen
+        if(this.x < this.w/2){
+            this.x = this.w/2
+            this.vx = 0
         }
     }
+      
+}
+
+//Main Screen
+gameStates[0] = function(){
+    ctx.save()
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+    ctx.fillText("Asteroid Avoider", canvas.width/2, canvas.height/2-30)
+    ctx.font = "15px Arial"
+    ctx.fillText("Press Space to Start", canvas.width/2, canvas.height/2 + 20)
+    ctx.restore()
 
 }
 
-//for loop to instantiate asteroids for game
-for(var i = 0; i<numAsteroids; i++){
-    asteroids[i] = new Asteroid();
-}
+//Game Screen
+gameStates[1] = function(){
+    //code for displaying score
+    ctx.save()
+    ctx.font = "15px Arial"
+    ctx.fillStyle = "white"
+    ctx.fillText("Score: " + score.toString(), canvas.width - 150, 30)
+    ctx.restore()
 
-function main(){
-    //clear the canvas
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    //draw score to screen
-    ctx.save();
-    ctx.font = "15px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText(`Score: ${score}`, canvas.width - 150, 30);
-    ctx.restore();
-
-    //vertical movement
+    //Vertical 
     if(ship.up){
-        ship.vy = -10;
+        ship.vy = -10
     }else{
-        ship.vy = 3;
+        ship.vy = 3
     }
-    //horizontal movement
+    
+    //Horizontal Movement
     if(ship.left){
-        ship.vx = -3;
+        ship.vx = -3
     }else if(ship.right){
-        ship.vx = 3;
+        ship.vx = 3
     }else{
-        ship.vx=0;
+        ship.vx = 0
     }
-    for(var i = 0; i<asteroids.length; i++){
-        var dX = ship.x - asteroids[i].x;
-        var dY = ship.y - asteroids[i].y;
-        var distance = Math.sqrt((dX*dX) + (dY*dY));
 
-        if(detectCollision(distance, (ship.height/2 + asteroids[i].radius))){
-            //console.log("Hit Asteroid");
-            //alert("Hit Asteroid");
-            gameOver = true;
+    //Loops through all asteroids and can check their position
+    for(var i = 0; i < asteroids.length; i++){
+        var dX = ship.x - asteroids[i].x
+        var dY = ship.y - asteroids[i].y
+        var distance = Math.sqrt((dX*dX)+(dY*dY))
+
+        if(detectCollision(distance, (ship.h/2 + asteroids[i].radius))){
+            console.log("hit asteroid")
+            gameOver = true
+            currentState = 2
+            main()
+            
         }
 
 
-        if(asteroids[i].y>canvas.height + asteroids[i].radius){
-            asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) - canvas.height;
-            asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius);
+        if(asteroids[i].y > canvas.height + asteroids[i].radius){
+            asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius)
+            asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) -  canvas.height
         }
-        asteroids[i].y += asteroids[i].vy
-        asteroids[i].drawAsteroid();
+        if(!gameOver){
+            asteroids[i].y += asteroids[i].vy
+            asteroids[i].drawAsteroid()
+        }
     }
-
-    //draw the ship
-    ship.moveShip();
-    ship.drawShip();
     if(!gameOver){
-        //refresh the screen
-        timer = requestAnimationFrame(main);
+        ship.move()
+        ship.drawShip()
     }
 
     while(asteroids.length < numAsteroids){
-        asteroids.push(new Asteroid());
+        asteroids.push(new Asteroid())
     }
-
 }
 
-//utility functions
+//Game Over
+gameStates[2] = function(){
+    if(score > highScore){
+        //set a new high score
+        highScore = score
+        ctx.save()
+        ctx.font = "30px Arial"
+        ctx.fillStyle = "white"
+        ctx.textAlign = "center"
+        ctx.fillText("Game Over, your high score score was: " + score.toString() , canvas.width/2, canvas.height/2-60)
+        ctx.fillText("Your new high score is: " + highScore.toString() , canvas.width/2, canvas.height/2-30)
+        ctx.fillText("New Record", canvas.width/2, canvas.height/2)
+        ctx.font = "15px Arial"
+        ctx.fillText("Press Space to Play Again", canvas.width/2, canvas.height/2 + 20)
+        ctx.restore()
 
-function randomRange(high,low){
-    return Math.random() * (high-low) + low;
+    }else{
+        //keep same score new high score
+        ctx.save()
+        ctx.font = "30px Arial"
+        ctx.fillStyle = "white"
+        ctx.textAlign = "center"
+        ctx.fillText("Game Over, your score was: " + score.toString() , canvas.width/2, canvas.height/2-60)
+        ctx.fillText("Your high score is: " + highScore.toString() , canvas.width/2, canvas.height/2-30)
+        ctx.font = "15px Arial"
+        ctx.fillText("Press Space to Play Again", canvas.width/2, canvas.height/2 + 20)
+        ctx.restore()
+    }
+    
+   
 }
 
+function main(){
+    //clear canvas 
+    //shipY-=1
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+
+    gameStates[currentState]()
+
+    if(!gameOver){
+        timer = requestAnimationFrame(main)
+    }
+    
+}
 
 function detectCollision(distance, calcDistance){
-    return distance < calcDistance;
+    return distance < calcDistance
 }
 
+//Timer for Score
 function scoreTimer(){
     if(!gameOver){
-        score++;
-
+        score++
+        //using modulus  that returns remainder of a decimal
+        //checks to see if remainder is divisble by 5
         if(score % 5 == 0){
-            numAsteroids += 5;
-            console.log(numAsteroids);
+            numAsteroids += 5
+            console.log(numAsteroids)
         }
-        //call scoreTimer every second
-        setTimeout(scoreTimer, 1000);
-    }
-}
 
-//temp call score function
-scoreTimer();
+        setTimeout(scoreTimer, 1000)
+    }
+
+}
